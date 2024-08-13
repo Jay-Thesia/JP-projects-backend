e58c561d128671c1616269ddc0221daad5c07279
Jay Thesia <jayengineer.jp2410@gmail.com>
2024-08-13 19:24:20 +0530

    bcrypt import change to es type

--- src/modules/auth/services/auth.service.ts
+++ src/modules/auth/services/auth.service.ts
@@ -1,42 +1,43 @@
-import { logger } from "@configs/logger.config"
+import { logger } from "@configs/logger.config";
 import { LoginInterface, UserInterface } from "../interfaces/auth.interfaces";
 import { HttpException } from "@helpers/response/httpException";
-import * as bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
 import Auth from "@models/Auth.model";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 import { SECRET_KEY } from "@configs/env.config";
 
-
 const checkPassword = async (data: LoginInterface, user: UserInterface) => {
-    if (!user.password) throw new HttpException(400, 'PASSWORD_NOT_SET');
-    const isMatch = await bcrypt.compare(data.password, JSON.parse(JSON.stringify(user)).password);
-    if (isMatch) return true;
-    else throw new HttpException(400, 'PASSWORD_ERROR', null, true);
-  };
+  if (!user.password) throw new HttpException(400, "PASSWORD_NOT_SET");
+  const isMatch = await bcrypt.compare(
+    data.password,
+    JSON.parse(JSON.stringify(user)).password
+  );
+  if (isMatch) return true;
+  else throw new HttpException(400, "PASSWORD_ERROR", null, true);
+};
 
-  const createToken =async (user: UserInterface) => {
-    return jwt.sign({ email: user.email, userId: user._id }, SECRET_KEY, { expiresIn: '1d' });
-  };
+const createToken = async (user: UserInterface) => {
+  return jwt.sign({ email: user.email, userId: user._id }, SECRET_KEY, {
+    expiresIn: "1d",
+  });
+};
 
-export const loginService=async (data:LoginInterface)=>{
-    try {
-        const {email}=data
-       
-        const user:any = await Auth.findOne({email})
-      
+export const loginService = async (data: LoginInterface) => {
+  try {
+    const { email } = data;
 
-        if(user){
-            const passwordCheck=await checkPassword(data,user);
+    const user: any = await Auth.findOne({ email });
 
-            if(passwordCheck){
-                return {
-                    access_token:await createToken(user)
-                }
-            }
-        }else throw new HttpException(400, 'REGISTER_FIRST', null, true);
-      
-        
-    } catch (error) {
-        logger.error(`Error in login service ::: ${error}`)
-    }
-}
+    if (user) {
+      const passwordCheck = await checkPassword(data, user);
+
+      if (passwordCheck) {
+        return {
+          access_token: await createToken(user),
+        };
+      }
+    } else throw new HttpException(400, "REGISTER_FIRST", null, true);
+  } catch (error) {
+    logger.error(`Error in login service ::: ${error}`);
+  }
+};
